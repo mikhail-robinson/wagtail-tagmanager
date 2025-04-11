89a38e3ddb3b9b2efe93978e2d38bc9b14748ff1
Mikhail Robinson <126931597+mikhail-robinson@users.noreply.github.com>
2025-04-11 15:59:01 +1200

    ensure only tagged items with an existing model are counted in the ManagedTagViewSet queryset so the order on the page is correct

--- src/wagtail_tagmanager/viewsets.py
+++ src/wagtail_tagmanager/viewsets.py
@@ -1,4 +1,5 @@
-from django.db.models import Count
+from django.apps import apps
+from django.db.models import Count, Q
 from wagtail.admin.panels import FieldPanel
 from wagtail.snippets.models import register_snippet
 from wagtail.snippets.views.snippets import SnippetViewSet
@@ -13,7 +14,7 @@ class ManagedTagViewSet(SnippetViewSet):
     add_to_admin_menu = True
     menu_label = "Tags"
     menu_order = 500
-    list_display = ["name", "slug", "get_tagged_object_count"]
+    list_display = ["name", "slug", "object_count_number"]
     search_fields = ("name",)
     ordering = ["-object_count_number"]
 
@@ -24,8 +25,18 @@ class ManagedTagViewSet(SnippetViewSet):
     ]
 
     def get_queryset(self, request):
+        # Get all valid model names
+        valid_models = []
+        for app_config in apps.get_app_configs():
+            for model in app_config.get_models():
+                valid_models.append(model.__name__.lower())
+
         return self.model.objects.annotate(
-            object_count_number=Count("taggit_taggeditem_items", distinct=True)
+            object_count_number=Count(
+                "taggit_taggeditem_items",
+                filter=Q(taggit_taggeditem_items__content_type__model__in=valid_models),
+                distinct=True,
+            )
         )
 
 
